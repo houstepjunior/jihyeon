851bf439a501b1e4c13011192a1a9ee76d334f30
김지현 <jhkim@houstep.co.kr>
2021-08-11 07:27:42 +0900

    드디어 로그인성공

--- boiler-plate/models/user.js
+++ boiler-plate/models/user.js
@@ -34,9 +34,9 @@ const userSchema = mongoose.Schema({
     }
 })
 
-userSchema.pre('save', function( next ){
+userSchema.pre('save', function(next){
     var user = this;
-    if(user.isModified('password')){
+    if (user.isModified('password')){
         bcrypt.genSalt(saltRounds, function (err, salt) {
             if (err) return next(err)
             bcrypt.hash(user.password, salt, function (err, hash){
@@ -50,26 +50,31 @@ userSchema.pre('save', function( next ){
     }
 })
 
-userSchema.methods.comparePassword = function(plainPassword, cb){
+userSchema.methods.comparePassword = function (plainPassword, cb) {
 
-    //plainPasssword 1234567    암호화된 비밀번호 dkfdmkfmdkfmk
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if(err) return cb(err),
-        cb(null, isMatch)
+    //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
     })
 }
 
-userSchema.methods.generateToken = function(cb){
+userSchema.methods.generateToken = function (cb) {
     var user = this;
-    //jsonwebtoken 이용해서 token을 생성하기
+    // console.log('user._id', user._id)
+
+    // jsonwebtoken을 이용해서 token을 생성하기 
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token 
+    // -> 
+    // 'secretToken' -> user._id
+
     user.token = token
-    user.save(function(err,user){
-        if(err) return cb(err)
-        cb(null,user)
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
     })
 }
-
 const User = mongoose.model('User',userSchema)
 
 module.exports = {User}
